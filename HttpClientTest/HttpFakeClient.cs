ca00e21e9e2bab531a75cce17673c60eea748753
Leonardo <leonardo@kinetica-solutions.com>
2017-08-31 00:11:02 +0200

    initial commit

---
+++ HttpClientTest/HttpFakeClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientTest
+{
+    namespace Cellnex.Aquadocs.Mobile.Tests
+    {
+        /// <summary>
+        /// Fake class to mock HTTP requests, it needs the Send method to be mocked in order to be used
+        /// </summary>
+        public class FakeHttpMessageHandler : HttpMessageHandler
+        {
+            public virtual HttpResponseMessage Send(HttpRequestMessage request)
+            {
+                throw new NotImplementedException("Mock this please");
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+            {
+                return Task.FromResult(Send(request));
+            }
+        }
+    }
+}
---
+++ HttpClientTest/OffLineResponseHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientTest
+{
+    /// <summary>
+    /// Based on this code: https://stackoverflow.com/questions/22223223/how-to-pass-in-a-mocked-httpclient-in-a-net-test
+    /// </summary>
+    public class OffLineResponseHandler : DelegatingHandler
+    {
+        private readonly Dictionary<Uri, HttpResponseMessage> responses = new Dictionary<Uri, HttpResponseMessage>();
+        public void AddResponse(Uri uri, HttpResponseMessage responseMessage)
+        {
+            responses.Add(uri, responseMessage);
+        }
+        public void AddOkResponse(Uri uri, string content)
+        {
+            var message = new HttpResponseMessage(HttpStatusCode.OK);
+            message.Content = new StringContent(content);
+            responses.Add(uri, message);
+        }
+        public void AddOkResponse(Uri uri)
+        {
+            this.AddOkResponse(uri, string.Empty);
+        }
+        public void AddServerErrorResponse(Uri uri)
+        {
+            var message = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            responses.Add(uri, message);
+        }
+        public void RemoveAll()
+        {
+            this.responses.Clear();
+        }
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            if (responses.ContainsKey(request.RequestUri))
+            {
+                return responses[request.RequestUri];
+            }
+            else
+            {
+                return await Task.FromResult(new HttpResponseMessage(HttpStatusCode.NotFound) { RequestMessage = request });
+            }
+        }
+    }
+}
